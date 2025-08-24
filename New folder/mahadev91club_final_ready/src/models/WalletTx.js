edbe65e98cbbe443ac4648e5a85a0f805b85deb9
Mahadev91club <khushirajput8945@gmail.com>
2025-08-24 20:02:03 +0530

    Delete New folder/mahadev91club_final_ready directory

--- New folder/mahadev91club_final_ready/src/models/WalletTx.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const WalletTx = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  type: { type: String, enum: ['add', 'deduct'], required: true },
-  coins: { type: Number, required: true },
-  note: { type: String, default: '' },
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('WalletTx', WalletTx);
--- New folder/mahadev91club_final_ready/src/routes/pages.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => res.render('home', { title: 'Mahadev91Club' }));
-router.get('/login', (req, res) => res.render('login', { title: 'Login' }));
-router.get('/register', (req, res) => res.render('register', { title: 'Register' }));
-router.get('/game', (req, res) => res.render('game', { title: 'Color Game' }));
-
-module.exports = router;
